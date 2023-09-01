7491f299e0636282d7395fae7a517d50955888f2
CraftersVerse <138868427+CraftersVerse@users.noreply.github.com>
2023-09-01 14:08:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.chly0cgexfmi.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limmengleong-employee"
+customregion = "us-east-1"
+
